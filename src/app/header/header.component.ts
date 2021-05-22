fb987b5f371255994ee7b2343a52b25b296393f9
Manfrin92 <manfredd_10@hotmail.com>
2021-05-22 07:56:18 -0300

    feat: auth

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,12 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
 import { DataStorageService } from 'src/shared/data-storage.service';
+import { AuthService } from '../auth/auth.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
 })
-export class HeaderComponent {
-  constructor(private dataStorageService: DataStorageService) {}
+export class HeaderComponent implements OnInit, OnDestroy {
+  isAuthenticated = false;
+  private userSub: Subscription;
+
+  constructor(
+    private dataStorageService: DataStorageService,
+    private authService: AuthService
+  ) {}
+
+  ngOnInit() {
+    this.userSub = this.authService.user.subscribe(
+      (user) => (this.isAuthenticated = !!user)
+    );
+  }
+
+  ngOnDestroy() {
+    this.userSub.unsubscribe();
+  }
 
   onSaveData() {
     this.dataStorageService.storeRecipes();
@@ -15,4 +33,6 @@ export class HeaderComponent {
   onFetchData() {
     this.dataStorageService.fetchRecipes().subscribe();
   }
+
+  onLogout() {}
 }
